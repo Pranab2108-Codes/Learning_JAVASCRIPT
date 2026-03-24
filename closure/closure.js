ba21a46dbb151919513844b3e0f6241909534bae
Pranab2108-Codes <pranab2108.codes@gmail.com>
2026-03-24 23:26:44 +0530

    closure.js

---
+++ closure/closure.js
@@ -0,0 +1,9 @@
+function init(){
+    let name = "Safari";
+    function displayName(){
+        console.log(name);             /* Required data got bind. */
+    }
+    return displayName;
+}
+let new_Function = init();
+new_Function();
